7543358ba3b5cae286c64ebdbe63a682ee2cbecd
islammd39e <islammdrahat308@gmail.com>
2023-03-25 15:52:54 +0600

    25/03/23

--- src/section/Main.jsx
+++ src/section/Main.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  userName: "",
+  email: "",
+  address: "",
+  address2: "",
+  country: "",
+  state: "",
+  zip: "",
+  paymentMethod: "",
+  nameOnCard: "",
+  cardNumber: "",
+  expiration: "",
+  cvv: ""
+
+}
+
+
+const validationSchema = Yup.object({
+  firstName: Yup.string().min(6, 'Minimum 6 characters').required('required'),
+  lastName: Yup.string().min(2, 'Minimum 2 characters').required('required'),
+  userName: Yup.string().matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,20})/,
+    "Must Contain 8 Characters and Maximum 20 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+  ).required('required'),
+  email: Yup.string().email('Invalid email address').required('required'),
+  address: Yup.string().required('required'),
+  address2: Yup.string(),
+  country: Yup.string().required('required'),
+  state: Yup.string().required('required'),
+  zip: Yup.number().required('required'),
+  paymentMethod: Yup.string().required('required'),
+  nameOnCard: Yup.string().required('required'),
+  cardNumber: Yup.number().required('required'),
+  expiration: Yup.string().required('required'),
+  cvv: Yup.number().required('required')
+
+})
+
 
 const Main = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => {
-    console.log(data)
-  };
-  console.log(errors);
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    }
+  })
+
   return (
     <div>
       <div className='row m-0 g-5'>
@@ -58,67 +104,87 @@ const Main = () => {
         </div>
         <div className="col-md-7 col-lg-8">
           <h4 className="mb-3">Billing address</h4>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={formik.handleSubmit} autoComplete="off">
             <div className="row g-3">
               <div className="col-sm-6">
-                <label className="form-label">First name</label>
-                <input type="text" className="form-control" {...register("FirstName", { required: true, maxLength: 80 })} />
-                {errors.FirstName && <small className='text-danger d-block pt-1'>Valid first name is required.</small>}
+                <label htmlFor='firstName' className="form-label">First name</label>
+                <input id='firstName' type="text"  {...formik.getFieldProps('firstName')} className="form-control" />
+                {formik.touched.firstName && formik.errors.firstName ? (
+                  <small className='text-danger'>{formik.errors.firstName}</small>
+                ) : null}
               </div>
 
               <div className="col-sm-6">
-                <label className="form-label">Last name</label>
-                <input type="text" className="form-control" {...register("LastName", { required: true, maxLength: 80 })} />
-                {errors.LastName && <small className='text-danger d-block pt-1'>Valid Last name is required.</small>}
+                <label htmlFor='lastName' className="form-label">Last name</label>
+                <input id='lastName' {...formik.getFieldProps('lastName')} type="text" className="form-control" />
+                {formik.touched.lastName && formik.errors.lastName ? (
+                  <small className='text-danger'>{formik.errors.lastName}</small>
+                ) : null}
               </div>
 
               <div className="col-12">
                 <label className="form-label">Username</label>
                 <div className="input-group">
                   <span className="input-group-text">@</span>
-                  <input type="text" className="form-control" placeholder="Username" {...register("Username", { required: true, maxLength: 80 })} />
+                  <input id='userName' {...formik.getFieldProps('userName')} type="text" className="form-control" placeholder="Username" />
                 </div>
-                {errors.Username && <small className='text-danger d-block pt-1'>Your username is required.</small>}
+                {formik.touched.userName && formik.errors.userName ? (
+                  <small className='text-danger'>{formik.errors.userName}</small>
+                ) : null}
               </div>
 
               <div className="col-12">
                 <label className="form-label">Email <span className="text-muted">(Optional)</span></label>
-                <input type="email" className="form-control" placeholder="you@example.com" {...register("Email", { required: false, maxLength: 80 })} />
+                <input id='email' {...formik.getFieldProps('email')} type="email" className="form-control" placeholder="you@example.com" />
+                {formik.touched.email && formik.errors.email ? (
+                  <small className='text-danger'>{formik.errors.email}</small>
+                ) : null}
               </div>
 
               <div className="col-12">
                 <label className="form-label">Address</label>
-                <input type="text" className="form-control" placeholder="1234 Main St" {...register("Address", { required: true, maxLength: 80 })} />
-                {errors.Address && <small className='text-danger d-block pt-1'>Please enter your shipping address.</small>}
+                <input {...formik.getFieldProps('address')}  id='address' type="text" className="form-control" placeholder="1234 Main St" />
+                {formik.touched.address && formik.errors.address ? (
+                  <small className='text-danger'>{formik.errors.address}</small>
+                ) : null}
               </div>
 
               <div className="col-12">
                 <label className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
-                <input type="text" className="form-control" placeholder="Apartment or suite" {...register("Address2", { required: false, maxLength: 80 })} />
+                <input {...formik.getFieldProps('address2')}  id='address2' type="text" className="form-control" placeholder="Apartment or suite" />
+                {formik.touched.address2 && formik.errors.address2 ? (
+                  <small className='text-danger'>{formik.errors.address2}</small>
+                ) : null}
               </div>
 
               <div className="col-md-5">
                 <label className="form-label">Country</label>
-                <select {...register("Country", { required: true })} className="form-select">
+                <select {...formik.getFieldProps('country')} className="form-select">
                   <option>Choose...</option>
                   <option>United States</option>
                 </select>
-                {errors.Country && <span className='text-danger d-block pt-1'>Please select a valid country.</span>}
+                {formik.touched.country && formik.errors.country ? (
+                  <small className='text-danger'>{formik.errors.country}</small>
+                ) : null}
               </div>
 
               <div className="col-md-4">
                 <label className="form-label">State</label>
-                <select {...register("State", { required: true })} aria-invalid={errors.State ? "true" : "false"}  className="form-select" >
+                <select {...formik.getFieldProps('state')} className="form-select" >
                   <option>Choose...</option>
                   <option>California</option>
                 </select>
-              {errors.State?.type === 'required' && <small className='text-danger d-block pt-1'>Please provide a valid state.</small>}
+                {formik.touched.state && formik.errors.state ? (
+                  <small className='text-danger'>{formik.errors.state}</small>
+                ) : null}
               </div>
 
               <div className="col-md-3">
                 <label className="form-label">Zip</label>
-                <input type="number" className="form-control" {...register("Zip", { required: true })}/>
-                {errors.Zip && <small className='text-danger d-block pt-1'>Zip code required.</small>}
+                <input {...formik.getFieldProps('zip')}  id='zip' type="number" className="form-control" />
+              {formik.touched.zip && formik.errors.zip ? (
+                  <small className='text-danger'>{formik.errors.zip}</small>
+                ) : null}
               </div>
             </div>
 
@@ -140,15 +206,15 @@ const Main = () => {
 
             <div className="my-3">
               <div className="form-check">
-                <input name="paymentMethod" type="radio" className="form-check-input" {...register("paymentMethod", { required: false })} value="Credit card"/>
+                <input {...formik.getFieldProps('paymentMethod')}  name="paymentMethod" type="radio" className="form-check-input" defaultValue={'Credit Card'} value="Credit card" />
                 <label className="form-check-label" >Credit card</label>
               </div>
               <div className="form-check">
-                <input name="paymentMethod" type="radio" className="form-check-input" {...register("paymentMethod", { required: false })} value="Debit card"/>
+                <input {...formik.getFieldProps('paymentMethod')} name="paymentMethod" type="radio" className="form-check-input" value="Debit card" />
                 <label className="form-check-label" >Debit card</label>
               </div>
               <div className="form-check">
-                <input name="paymentMethod" type="radio" className="form-check-input" {...register("paymentMethod", { required: false })} value="Paypal"/>
+                <input {...formik.getFieldProps('paymentMethod')} name="paymentMethod" type="radio" className="form-check-input" value="Paypal" />
                 <label className="form-check-label" >PayPal</label>
               </div>
             </div>
@@ -156,33 +222,41 @@ const Main = () => {
             <div className="row gy-3">
               <div className="col-md-6">
                 <label className="form-label">Name on card</label>
-                <input type="text" className="form-control" {...register("NameOnCard", { required: true })}/>
+                <input {...formik.getFieldProps('nameOnCard')}  id='nameOnCard' type="text" className="form-control" />
                 <small className="text-muted">Full name as displayed on card</small>
-                {errors.NameOnCard && <small className='text-danger d-block pt-1'>Name on card is required.</small>}
+                {formik.touched.nameOnCard && formik.errors.nameOnCard ? (
+                  <small className='text-danger'>{formik.errors.nameOnCard}</small>
+                ) : null}
               </div>
 
               <div className="col-md-6">
                 <label className="form-label">Credit card number</label>
-                <input type="number" className="form-control" {...register("CardNumber", { required: true })}/>
-                {errors.CardNumber && <small className='text-danger d-block pt-1'>Credit card number is required.</small>}
+                <input {...formik.getFieldProps('cardNumber')}  id='cardNumber' type="number" className="form-control" />
+                {formik.touched.cardNumber && formik.errors.cardNumber ? (
+                  <small className='text-danger'>{formik.errors.cardNumber}</small>
+                ) : null}
               </div>
 
               <div className="col-md-3">
                 <label className="form-label">Expiration</label>
-                <input type="text" className="form-control" {...register("Expiration", { required: true })}/>
-                {errors.Expiration && <small className='text-danger d-block pt-1'>Expiration date required.</small>}
+                <input {...formik.getFieldProps('expiration')} id='expiration' type="text" className="form-control" />
+                {formik.touched.expiration && formik.errors.expiration ? (
+                  <small className='text-danger'>{formik.errors.expiration}</small>
+                ) : null}
               </div>
 
               <div className="col-md-3">
                 <label className="form-label">CVV</label>
-                <input type="number" className="form-control" {...register("Cvv", { required: true })}/>
-                {errors.Cvv && <small className='text-danger d-block pt-1'>Security code required.</small>}
+                <input {...formik.getFieldProps('cvv')}  id='cvv' type="number" className="form-control" />
+                {formik.touched.cvv && formik.errors.cvv ? (
+                  <small className='text-danger'>{formik.errors.cvv}</small>
+                ) : null}
               </div>
             </div>
 
             <hr className="my-4" />
 
-            <button type="submit" className='w-100 btn btn-primary btn-lg'>Continue to checkout</button>
+            <button type='submit' className='w-100 btn btn-primary btn-lg'>Continue to checkout</button>
           </form>
         </div>
       </div>
